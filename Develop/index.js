9c7ddb9aaa63f8b4b41963e2fb69cbfb60f3787e
Experiment 626 <rcourt@live.com.au>
2021-01-22 21:18:33 +1100

    Organised folders after npm testing

--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,45 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-
-const questions = () => {
-  return inquirer.prompt([ 
-      {
-        type: 'input',
-        name: 'Project title',
-        message: 'What is the title of your project/application?',
-      },
-      {
-        type: 'input',
-        name: 'Description',
-        message: 'Please write a description for your application.',
-      },
-      {
-        type: 'input',
-        name: 'Installation',
-        message: 'Please outline the steps for installing the application.',
-      },
-      {
-        type: 'input',
-        name: 'Usage',
-        message: 'Please detail the necessary usage information for the potential user.',
-      },
-      {
-        type: 'input',
-        name: 'Contributing',
-        message: 'Please explain the process whereby users may be able to contribute to the project. (NB - It may also help to specify any aspects of the project where you would welcome user input/feedback.)',
-      },
-      {
-        type: 'input',
-        name: 'Tests',
-        message: 'Please enter any test instructions that you wish the user to know.',
-      },
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'Project title',
+      message: 'What is the title of your project/application?',
+    },
+    {
+      type: 'input',
+      name: 'Description',
+      message: 'Please write a description for your application.',
+    },
+    {
+      type: 'input',
+      name: 'Installation',
+      message: 'Please outline the steps for installing the application.',
+    },
+    {
+      type: 'input',
+      name: 'Usage',
+      message: 'Please detail the necessary usage information for the potential user.',
+    },
+    {
+      type: 'input',
+      name: 'Contributing',
+      message: 'Please explain the process whereby users may be able to contribute to the project. (NB - It may also help to specify any aspects of the project where you would welcome user input/feedback.)',
+    },
+    {
+      type: 'input',
+      name: 'Tests',
+      message: 'Please enter any test instructions that you wish the user to know.',
+    },
   ]);
 };
-    // Project title 
-        // NB - Project title becomes the title of the README
-    
-    // Description - 'Description'
-    // Installation instructions - 'Installation' 
-    // Usage information - 'Usage'
-    // Contribution guidelines - 'Contributing'
-    // Test instructions - 'Tests'
-        // NB - Above information is added to sections of the Readme file.
 
-    // Table of Contents - 'Table of Contents'
-        // NB - links from table of contents take viewer to that section of the readme file.
-        
-    // License - 'License'
-        // NB - license for application is chosen from a list of options;
-            // badge for that license is added near top of readme file.
-            // notice is added to the section of the readme called 'License'.
-            // explanation of license under which the project is covered appears
-            // in this section.
-    // Questions - 'Questions'
-        // NB - user's github username and github profile page link is requested.
-            // username and link appear within the section entitled 'Questions'.
-            // Questions sections contains instructions on how to reach user with additional questions.
-
-// Function to write README file
-function writeToFile(fileName, data) {}
-const writeFileAsync = util.promisify(fs.writeFile);
-const generateReadmeFile = (answers) =>
+const generateHTML = (answers) =>
 `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -91,21 +63,20 @@ const generateReadmeFile = (answers) =>
 </body>
 </html>`;
 
-// Function to initialize app
-  console.log('hi');
-  try {
-    const answers = await questions();
+// Bonus using async/await and try/catch
+const init = async () => {
+  console.log('\nWelcome to "Readme File Generator"\n\n   IMPORTANT: Please note that your responses to the prompts below will form the content of your readme file.');
+  // try {
+  //   const answers = await promptUser();
 
-    const readme = generateReadmeFile(answers);
+  //   const html
 
-    await writeFileAsync('readme2.md', readme);
+init(); = generateHTML(answers);
 
-    console.log('Successfully wrote to readme2.me');
-  } catch (err) {
-    console.log(err);
+  //   await writeFileAsync('index.html', html);
+
+  //   console.log('Successfully wrote to index.html');
+  // } catch (err) {
+  //   console.log(err);
+  // }
 };
-
-function init() {}
-
-// Function call to initialize app
-init();
---
+++ Develop/roughIndex.js
@@ -0,0 +1,109 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+
+// An array of questions for user input
+
+const questions = () => {
+  return inquirer.prompt([ 
+      {
+        type: 'input',
+        name: 'Project title',
+        message: 'What is the title of your project/application?',
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Please write a description for your application.',
+      },
+      {
+        type: 'input',
+        name: 'Installation',
+        message: 'Please outline the steps for installing the application.',
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'Please detail the necessary usage information for the potential user.',
+      },
+      {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Please explain the process whereby users may be able to contribute to the project. (NB - It may also help to specify any aspects of the project where you would welcome user input/feedback.)',
+      },
+      {
+        type: 'input',
+        name: 'Tests',
+        message: 'Please enter any test instructions that you wish the user to know.',
+      },
+  ]);
+};
+    // Project title 
+        // NB - Project title becomes the title of the README
+    
+    // Description - 'Description'
+    // Installation instructions - 'Installation' 
+    // Usage information - 'Usage'
+    // Contribution guidelines - 'Contributing'
+    // Test instructions - 'Tests'
+        // NB - Above information is added to sections of the Readme file.
+
+    // Table of Contents - 'Table of Contents'
+        // NB - links from table of contents take viewer to that section of the readme file.
+        
+    // License - 'License'
+        // NB - license for application is chosen from a list of options;
+            // badge for that license is added near top of readme file.
+            // notice is added to the section of the readme called 'License'.
+            // explanation of license under which the project is covered appears
+            // in this section.
+    // Questions - 'Questions'
+        // NB - user's github username and github profile page link is requested.
+            // username and link appear within the section entitled 'Questions'.
+            // Questions sections contains instructions on how to reach user with additional questions.
+
+// Function to write README file
+function writeToFile(fileName, data) {}
+const writeFileAsync = util.promisify(fs.writeFile);
+const readmeFile = (answers) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+
+// Function to initialize app
+  console.log('hi');
+  try {
+    const answers = await questions();
+
+    const generateReadme = readmeFile(answers);
+
+    fs.writeFileAsync('readme2.md', generateReadme.trim(), (error) => {
+      console.log('Successfully wrote to readme2.me');
+    });
+}
+
+function init() {}
+
+// Function call to initialize app
+init();
