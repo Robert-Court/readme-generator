e31116f15390f56f99081b4ec6c17c182e327c82
Experiment 626 <rcourt@live.com.au>
2021-01-22 21:37:13 +1100

    Readme file generating after prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const { generateReadme } = require("./utils/generateReadme")
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const promptUser = () => {
+const askQuestions = () => {
   return inquirer.prompt([
     {
       type: 'input',
@@ -39,44 +40,19 @@ const promptUser = () => {
   ]);
 };
 
-const generateHTML = (answers) =>
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+
+
 
 // Bonus using async/await and try/catch
 const init = async () => {
   console.log('\nWelcome to "Readme File Generator"\n\n   IMPORTANT: Please note that your responses to the prompts below will form the content of your readme file.');
-  // try {
-  //   const answers = await promptUser();
+    const answers = await askQuestions();
 
-  //   const html
+    const generatedReadme = generateReadme(answers);
 
-init(); = generateHTML(answers);
+    fs.writeFile('readme.md', generatedReadme.trim(), (error) => {
+      console.log('Successfully wrote to readme file.');
+  });
+}
 
-  //   await writeFileAsync('index.html', html);
-
-  //   console.log('Successfully wrote to index.html');
-  // } catch (err) {
-  //   console.log(err);
-  // }
-};
+init ();
---
+++ Develop/utils/generateReadme.js
@@ -0,0 +1,10 @@
+const generateReadme = (answers) => {
+    // NB - Readme file structure is located within the template literal below
+    return `
+
+    `
+}
+
+module.exports = {
+        generateReadme
+}
